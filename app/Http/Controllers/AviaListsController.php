3ea887102d443a35e4ab92c89736112b815fa80a
Madiyar <huistowns@gmail.com>
2020-11-30 14:15:25 +0600

    поиск

--- app/Http/Controllers/AviaListsController.php
+++ app/Http/Controllers/AviaListsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Livewire\Flights;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Team;
@@ -23,10 +24,22 @@ class AviaListsController extends Controller
         // ->join('airlines', 'flights.airline_id', '=', 'airlines.id')
         // ->select('flights.*', 'cities.name', 'airlines.name')
         // ->get();
-        $city_from_req = City::where('name', $request->from_city)->get();
-        $city_to_req = City::where('name', $request->to_city)->get();
-        $flights = Flight::where('city_from_id', $city_from_req)
-        ->where('city_to_id', $city_to_req)->get();
-        return view('avia_list', ['flights' => $flights]);
+        // $city_from_req = City::where('name', $request->from_city)->get();
+        // $city_to_req = City::where('name', $request->to_city)->get();
+        // $flights = Flight::where('city_from_id', $city_from_req)
+        // ->where('city_to_id', $city_to_req)->get();
+        $city = City::where('name', $request->to_city)->first();
+        $flights = DB::table('flights')
+        ->join('cities', 'flights.city_from_id', '=', 'cities.id')
+        ->where('cities.name', $request->from_city)
+        ->where('flights.flight_date', '>=', $request->date_to)
+        ->where('flights.city_to_id', $city->id)
+        ->get();
+
+        
+        // $flights = Flight::where('id', $cities->id)->get();
+
+
+        return ($flights);
     }
 }
