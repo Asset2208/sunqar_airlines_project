0347cea6a34d306ce4151df2a939c6b306fa32c2
Asset2208 <57480799+Asset2208@users.noreply.github.com>
2020-12-24 15:06:27 +0600

    Добавил API погоды

--- resources/views/avia_list.blade.php
+++ resources/views/avia_list.blade.php
@@ -2,21 +2,27 @@
 
 @section('styles')
 <link rel="stylesheet" href="{{ url('css/avia_list.css') }}">
+<script
+  src="https://code.jquery.com/jquery-3.5.1.js"
+  integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+  crossorigin="anonymous"></script>
 @endsection
 
 @section('content')
-<section class="avia-list-result">
+<section class="avia-list-result" style="margin-top: 100px;">
+    <div id="weatherFrom"></div>
+    <div id="weatherTo"></div>
     @foreach($flights as $flight)
     <div class="flight" style="background: url('img/avia_list/avia-bg.jpg');" >
         <div class="flight-header">
             <p class="from">
                 <span class="subtitle">Откуда:</span>
-                <span>{{ $flight->city_from->name}}</span>
+                <span id="from_city">{{ $flight->city_from->name}}</span>
             </p>
 
             <p class="to">
                 <span class="subtitle">Куда:</span>
-                <span>{{ $flight->city_to->name }}</span>
+                <span id="to_city">{{ $flight->city_to->name }}</span>
             </p>
         </div>
         <div class="flight-time">
@@ -48,6 +54,45 @@
     </div> 
     @endforeach
     
+    <script type="text/javascript">
+        window.onload = function() {
+            var to_city = document.getElementById('to_city').textContent;
+            var from_city = document.getElementById('from_city').textContent;
+            var key = 'bb9109c347eed1a26479c2f6b36d3eeb';
+
+            $.ajax({
+                    url: 'http://api.openweathermap.org/data/2.5/weather',
+                    dataType: 'json',
+                    type: 'GET',
+                    data: {q: to_city, appid: key, units: 'metric'},
+
+                    success: function (data) {
+                        var wf = '';
+                        $.each(data.weather, function (index, val) {
+                            wf += '<p><b>' + data.name + "</b><img src=http://openweathermap.org/img/wn/" + val.icon + "@2x.png" + "></p>"+
+                            data.main.temp + '&deg;C' + ' | ' + val.main + ", " + val.description
+                        })
+                        $("#weatherTo").html(wf);
+                    }
+                });
+            $.ajax({
+                url: 'http://api.openweathermap.org/data/2.5/weather',
+                dataType: 'json',
+                type: 'GET',
+                data: {q: from_city, appid: key, units: 'metric'},
+
+                success: function (data) {
+                    var wf = '';
+                    $.each(data.weather, function (index, val) {
+                        wf += '<p><b>' + data.name + "</b><img src=http://openweathermap.org/img/wn/" + val.icon + "@2x.png" + "></p>"+
+                        data.main.temp + '&deg;C' + ' | ' + val.main + ", " + val.description
+                    })
+                    $("#weatherFrom").html(wf);
+                }
+            });
+    }
+    </script>
+        
     @if (isset($flights_back))
         <hr>
         <h2 class="back">Назад</h2>
