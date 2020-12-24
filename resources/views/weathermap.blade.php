be6e6b850cb9133922eabf31f154648254127dc0
Asset2208 <57480799+Asset2208@users.noreply.github.com>
2020-12-24 15:39:46 +0600

    Рефакторинг

--- resources/views/weathermap.blade.php
+++ resources/views/weathermap.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.weather')
 
 @section('weather')
-    <iframe src="https://openweathermap.org/weathermap?basemap=map&cities=true&layer=clouds&lat=33.2846&lon=58.1836&zoom=3" width="100%" height="100%" style="position:absolute; clip:rect(190px,1100px,800px,250px);
-            top:-160px;"></iframe>
+    <iframe src="https://openweathermap.org/weathermap?basemap=map&cities=true&layer=clouds&lat=33.2846&lon=58.1836&zoom=3" width="100%" height="100%" style="min-height: 100vh;"></iframe>
 @endsection
