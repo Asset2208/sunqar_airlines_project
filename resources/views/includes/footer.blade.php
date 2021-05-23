1f5ed5d6c048afbd7aae77883ddd10350b2b1da1
Madiyar <huistowns@gmail.com>
2021-05-23 12:14:48 +0600

    added footer some information

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -10,12 +10,15 @@
             <a href="#">Главная</a>
             <a href="#">Контакты</a>
             <a href="#">О нас</a>
+            <a href="#">Тех поддержка: +7(776)-288-49-55</a>
+            <a href="#">Онлайн консультация: +7(776)-288-49-55</a>
         </ul>
     </div>
     <div>
         <h2>SUNQAR AVIALINES</h2>
     </div>
     <div>
+        <p style="margin-left:20px">Наш адрес: Казахфильм 8, 88</p>
         <div id="map"></div>
     </div>
     </div>
